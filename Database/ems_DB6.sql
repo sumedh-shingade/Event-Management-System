c21b2cb745054fe334feb5ce1d0e6bb18ea7cdce
sumeetwd25 <sumeetwd25@gmail.com>
2023-08-25 09:55:46 +0530

    temporarily using hardcoded email_id in react profile.js

---
+++ Database/ems_DB6.sql
@@ -0,0 +1,68 @@
+
+
+CREATE TABLE venue (
+    venue_id INT PRIMARY KEY auto_increment,
+    name VARCHAR(255),
+    address VARCHAR(255),
+    location VARCHAR(255)
+);
+
+CREATE TABLE catering (
+    catering_id INT PRIMARY KEY auto_increment,
+   menu varchar(255)
+);
+
+
+CREATE TABLE decoration (
+    decoration_id INT PRIMARY KEY auto_increment,
+decor_type varchar(255)
+);
+
+CREATE TABLE media (
+    media_id INT PRIMARY KEY auto_increment,
+   media_type varchar(255)
+);
+
+
+CREATE TABLE accounts (
+    email_id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    mob_no VARCHAR(20),
+    role VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE bookings (
+    event_id INT PRIMARY KEY auto_increment,
+    event_name VARCHAR(255) NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    date DATE NOT NULL,
+    exp_attendee INT,
+    venue_id INT,
+    catering_id INT,
+    decoration_id INT,
+    media_id INT,
+    email_id VARCHAR(255),
+    FOREIGN KEY (venue_id) REFERENCES venue(venue_id),
+    FOREIGN KEY (catering_id) REFERENCES catering(catering_id),
+    FOREIGN KEY (decoration_id) REFERENCES decoration(decoration_id),
+    FOREIGN KEY (media_id) REFERENCES media(media_id)
+);
+
+
+
+CREATE TABLE login (
+    email_id VARCHAR(255),
+    password VARCHAR(255) NOT NULL
+);
+
+
+
+
+
+
+
+
+
