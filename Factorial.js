5ff6a5cddfd57c135b3a818271fafac07e666c42
Sushant Pawar <131552558+SushantPawar-7007@users.noreply.github.com>
2023-08-25 11:26:59 +0530

    Add files via upload

---
+++ Factorial.js
@@ -0,0 +1,17 @@
+// export default function Fact({num:a})
+// {
+//     let fact=1;
+//     let a;
+//     for(let i=a;i>=1;i--)
+//     {
+//         fact=fact*i;
+//     }
+      
+//     return <div>
+//       <p>fact of {a} is {fact}</p>
+
+
+//     </div>
+
+
+// }
